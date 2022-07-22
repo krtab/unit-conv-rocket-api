7211d951b522423104602a98908d0162ab20a306
Arthur Carcano <53921575+krtab@users.noreply.github.com>
2022-07-22 14:20:17 +0200

    init

---
+++ Readme.md
@@ -0,0 +1,25 @@
+Une API de conversion de quantités de masse entre différentes unités.
+
+Il y a un seul endpoint, `/`, qui accepte une requête POST dont le corps contient un JSON avec trois élements:
+
+- `quantity`: un `number` représentant la quantité numérique
+- `unit_from`: l'unité depuis laquelle on veut convertir (celle dans laquelle `quantity` est exprimé)
+- `unit_to`: l'unité vers laquelle on veut convertir
+
+Les unité valides sont:
+
+- `g`: gramme
+- `kg`: kilogramme
+- `lb`: livre
+- `metric ton`: une tonne métrique (1000kg)
+
+La réponse retournée sera un objet JSON de deux éléments:
+
+- `quantity`: la quantité convertie
+- `unit`: l'unité dans laquelle elle a été convertie (`unit_to` de la requête)
+
+Pour utiliser l'API localement, lancer le serveur avec `cargo run`, et utiliser curl comme client. Il est possible d'utiliser [jq](https://stedolan.github.io/jq/) après un pipe pour pretty-print le JSON retourné.
+
+```
+curl -s -X POST 127.0.0.1:8000 -H "Accept: application/json" -d '{"quantity": 17, "unit_from": "lb", "unit_to": "metric ton"}' 
+```
---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use rocket::{launch, post, routes, serde::json::Json, get};
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Copy, Deserialize, Serialize)]
+enum Unit {
+    #[serde(rename = "lb")]
+    Lb,
+    #[serde(rename = "g")]
+    G,
+    #[serde(rename = "kg")]
+    Kg,
+    #[serde(rename = "metric ton")]
+    MTon,
+}
+
+impl Unit {
+    const fn rate_to_grams(self) -> f64 {
+        match self {
+            Unit::Lb => 453.59237,
+            Unit::G => 1.,
+            Unit::Kg => 1_000.,
+            Unit::MTon => 1_000_000.,
+        }
+    }
+}
+
+#[derive(Deserialize)]
+struct ApiRequest {
+    quantity: f64,
+    unit_from: Unit,
+    unit_to: Unit,
+}
+
+#[derive(Serialize)]
+struct Response {
+    quantity: f64,
+    unit: Unit,
+}
+
+#[post("/", data = "<request>")]
+fn api_convert(request: Json<ApiRequest>) -> Json<Response> {
+    let rate = request.unit_from.rate_to_grams() / request.unit_to.rate_to_grams();
+    Json(Response {
+        quantity: request.quantity * rate,
+        unit: request.unit_to,
+    })
+}
+
+#[get("/")]
+fn landing_page() -> &'static str {
+    include_str!("../Readme.md")
+}
+
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![api_convert, landing_page])
+}
